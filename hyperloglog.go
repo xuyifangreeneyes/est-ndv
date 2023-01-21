8d11994e630b9d47d4a7331795fe37215b2ed5ba
xuyifan <675434007@qq.com>
2023-01-21 19:59:11 +0800

    upd hll

--- hyperloglog.go
+++ hyperloglog.go
@@ -1,4 +1,4 @@
-// The code is copied from https://github.com/eclesh/hyperloglog/blob/master/hyperloglog.go
+// The code is mainly from https://github.com/eclesh/hyperloglog/blob/master/hyperloglog.go
 package main
 
 import (
@@ -11,14 +11,14 @@ var (
 )
 
 type HyperLogLog struct {
-	m         uint    // Number of registers
-	b         uint32  // Number of bits used to determine register index
+	m         uint32  // Number of registers
+	b         uint8   // Number of bits used to determine register index
 	alpha     float64 // Bias correction constant
 	registers []uint8
 }
 
-// Compute bias correction alpha_m.
-func get_alpha(m uint) (result float64) {
+// getAlpha computes bias correction alpha_m.
+func getAlpha(m uint32) (result float64) {
 	switch m {
 	case 16:
 		result = 0.673
@@ -32,56 +32,51 @@ func get_alpha(m uint) (result float64) {
 	return result
 }
 
-// Return a new HyperLogLog with the given number of registers. More
-// registers leads to lower error in your estimated count, at the
-// expense of memory.
+// NewHyperLogLog returns a new HyperLogLog with the given number of registers. More registers leads to lower error in
+// your estimated count, at the expense of memory.
 //
-// Choose a power of two number of registers, depending on the amount
-// of memory you're willing to use and the error you're willing to
-// tolerate. Each register uses one byte of memory.
+// Choose a power of two number of registers, depending on the amount of memory you're willing to use and the error
+// you're willing to tolerate. Each register uses one byte of memory.
 //
-// Approximate error will be:
-//
-//	1.04 / sqrt(registers)
-func New(registers uint) (*HyperLogLog, error) {
+// Approximate error will be: 1.04 / sqrt(registers)
+func NewHyperLogLog(registers uint32) (*HyperLogLog, error) {
 	if (registers & (registers - 1)) != 0 {
 		return nil, fmt.Errorf("number of registers %d not a power of two", registers)
 	}
 	h := &HyperLogLog{}
 	h.m = registers
-	h.b = uint32(math.Ceil(math.Log2(float64(registers))))
-	h.alpha = get_alpha(registers)
+	h.b = uint8(math.Ceil(math.Log2(float64(registers))))
+	h.alpha = getAlpha(registers)
 	h.Reset()
 	return h, nil
 }
 
-// Reset all internal variables and set the count to zero.
+// Reset sets all registers to zero.
 func (h *HyperLogLog) Reset() {
 	h.registers = make([]uint8, h.m)
 }
 
-// Calculate the position of the leftmost 1-bit.
-func rho(val uint32, max uint32) uint8 {
-	r := uint32(1)
-	for val&0x80000000 == 0 && r <= max {
+// rho calculates the position of the leftmost 1-bit.
+func rho(val uint64, max uint8) uint8 {
+	r := uint8(1)
+	for val&0x8000000000000000 == 0 && r <= max {
 		r++
 		val <<= 1
 	}
-	return uint8(r)
+	return r
 }
 
-// Add to the count. val should be a 32 bit unsigned integer from a
-// good hash function.
-func (h *HyperLogLog) Add(val uint32) {
-	k := 32 - h.b
+// InsertHash inserts val into HyperLogLog. val should be a 64-bit unsigned integer from a good hash function.
+func (h *HyperLogLog) InsertHash(val uint64) {
+	k := 64 - h.b
 	r := rho(val<<h.b, k)
-	j := val >> uint(k)
+	j := val >> k
 	if r > h.registers[j] {
 		h.registers[j] = r
 	}
 }
 
-// Get the estimated count.
+// Count returns the estimated NDV.
 func (h *HyperLogLog) Count() uint64 {
 	sum := 0.0
 	m := float64(h.m)
@@ -89,34 +84,29 @@ func (h *HyperLogLog) Count() uint64 {
 		sum += 1.0 / math.Pow(2.0, float64(val))
 	}
 	estimate := h.alpha * m * m / sum
-	if estimate <= 5.0/2.0*m {
+	if estimate <= 2.5*m {
 		// Small range correction
-		v := 0
+		zeros := 0
 		for _, r := range h.registers {
 			if r == 0 {
-				v++
+				zeros++
 			}
 		}
-		if v > 0 {
-			estimate = m * math.Log(m/float64(v))
+		if zeros > 0 {
+			estimate = m * math.Log(m/float64(zeros))
 		}
-	} else if estimate > 1.0/30.0*exp32 {
-		// Large range correction
-		estimate = -exp32 * math.Log(1-estimate/exp32)
 	}
-	return uint64(estimate)
+	return uint64(estimate + 0.5)
 }
 
-// Merge another HyperLogLog into this one. The number of registers in
-// each must be the same.
-func (h1 *HyperLogLog) Merge(h2 *HyperLogLog) error {
-	if h1.m != h2.m {
-		return fmt.Errorf("number of registers doesn't match: %d != %d",
-			h1.m, h2.m)
+// Merge merges another HyperLogLog into this one. The number of registers in each must be the same.
+func (h *HyperLogLog) Merge(other *HyperLogLog) error {
+	if h.m != other.m {
+		return fmt.Errorf("number of registers doesn't match: %d != %d", h.m, other.m)
 	}
-	for j, r := range h2.registers {
-		if r > h1.registers[j] {
-			h1.registers[j] = r
+	for j, r := range other.registers {
+		if r > h.registers[j] {
+			h.registers[j] = r
 		}
 	}
 	return nil
